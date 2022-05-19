ff65e0de080ddc7c68d592e05c0d77b11840723c
Basil Njoga <Basilnjoga@gmail.com>
2022-05-19 13:19:25 +0300

    well written

---
+++ perfectcode.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the beginning of the program
+ * 
+ * This program simply prints out well arranged code
+ * 
+ * Return : nothing
+ */
+
+int main() 
+{
+   printf("Hello, World!");
+   return 0;
+}
